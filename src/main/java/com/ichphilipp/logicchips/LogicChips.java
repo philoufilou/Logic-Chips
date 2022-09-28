5707e3cd6463888e00066e018dd71aa541b57d87
IchPhilipp <philippleyrerpl@gmail.com>
2022-09-29 00:45:18 +0200

    Changed Mod ID

---
+++ src/main/java/com/ichphilipp/logicchips/LogicChips.java
@@ -0,0 +1,44 @@
+package com.ichphilipp.logicchips;
+
+import com.ichphilipp.logicchips.allblocks.AllBlocks;
+import com.ichphilipp.logicchips.allitems.AllItems;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.RenderTypeLookup;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.jetbrains.annotations.NotNull;
+
+@Mod(LogicChips.MOD_ID)
+public class LogicChips {
+    public static final String MOD_ID = "logicchips";
+    public static final Logger LOGGER = LogManager.getLogger();
+    public static final Config config = new Config();
+    public static final ItemGroup ITEM_GROUP = (new ItemGroup(MOD_ID) {
+        @Override
+        public @NotNull ItemStack makeIcon() {
+            return new ItemStack(AllItems.AND_GATE.get());
+        }
+    });
+    public LogicChips() {
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, LogicChips.config.defaultConfig);
+
+        AllItems.register(eventBus);
+        AllBlocks.register(eventBus);
+
+        eventBus.addListener(this::doClientStuff);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+    private void doClientStuff(final FMLClientSetupEvent event) {
+        event.enqueueWork(() -> RenderTypeLookup.setRenderLayer(AllBlocks.GATE_FRAME.get(), RenderType.cutout()));
+    }
+}
---
+++ src/main/java/com/ichphilipp/logicchips/allitems/Chip.java
@@ -0,0 +1,31 @@
+package com.ichphilipp.logicchips.allitems;
+
+import com.ichphilipp.logicchips.LogicChips;
+import com.ichphilipp.logicchips.utils.*;
+import com.ichphilipp.logicchips.allblocks.ChipFrame;
+import com.ichphilipp.logicchips.utils.GateFrameTypes;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.NotNull;
+import javax.annotation.Nullable;
+import java.util.List;
+public class Chip extends Item {
+    public Chip(Properties properties, GateFrameTypes types, TriFunction<Boolean,Boolean,Boolean,Boolean> TRI, int[] ints) {
+        super(properties);
+        ChipFrame.add(types,this, TRI, ints);
+    }
+    @Override
+    public void appendHoverText(@NotNull ItemStack pStack, @Nullable World pLevel, @NotNull List<ITextComponent> pTooltipComponents, @NotNull ITooltipFlag pIsAdvanced) {
+        if(Screen.hasShiftDown()) {
+            pTooltipComponents.add(new TranslationTextComponent(String.format("tooltip.%s.%s", LogicChips.MOD_ID, pStack.getItem())));
+        } else {
+            pTooltipComponents.add(new TranslationTextComponent("tooltip.shift"));
+        }
+        super.appendHoverText(pStack, pLevel, pTooltipComponents, pIsAdvanced);
+    }
+}
