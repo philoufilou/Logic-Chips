68951031ddd8294af56973fbbdbe66fee06249fc
IchPhilipp <philippleyrerpl@gmail.com>
2022-09-30 01:19:41 +0200

    ItemBlockRenderTypes Added

--- src/main/java/com/ichphilipp/logicchips/LogicChips.java
+++ src/main/java/com/ichphilipp/logicchips/LogicChips.java
@@ -3,11 +3,14 @@ package com.ichphilipp.logicchips;
 import com.ichphilipp.logicchips.allblocks.AllBlocks;
 import com.ichphilipp.logicchips.allitems.AllItems;
 import com.mojang.logging.LogUtils;
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
@@ -23,13 +26,15 @@ public class LogicChips {
         }
     };
     public LogicChips(){
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        AllItems.register(eventBus);
+        AllBlocks.register(eventBus);
 
-        AllItems.register(modEventBus);
-        AllBlocks.register(modEventBus);
-
-
+        eventBus.addListener(this::doClientStuff);
         MinecraftForge.EVENT_BUS.register(this);
+    }
+    private void doClientStuff(final FMLClientSetupEvent event) {
+        event.enqueueWork(() -> ItemBlockRenderTypes.setRenderLayer(AllBlocks.GATE_FRAME.get(), RenderType.cutout()));
     }
 }
