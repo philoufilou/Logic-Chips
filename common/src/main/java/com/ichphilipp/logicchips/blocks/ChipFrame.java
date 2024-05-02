8c03a679e7360fba03ec520299557fe29409e2d5
ZZZank <3410764033@qq.com>
2024-05-02 12:45:42 +0800

    fix cherry-picking

--- common/src/main/java/com/ichphilipp/logicchips/blocks/ChipFrame.java
+++ common/src/main/java/com/ichphilipp/logicchips/blocks/ChipFrame.java
@@ -17,6 +17,7 @@ import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
