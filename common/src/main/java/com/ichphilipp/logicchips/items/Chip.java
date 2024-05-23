96dc76b22eda4399f5c8848a68f7d98350bb9697
ZZZank <3410764033@qq.com>
2024-05-23 17:38:18 +0800

    fix tooltips

--- common/src/main/java/com/ichphilipp/logicchips/items/Chip.java
+++ common/src/main/java/com/ichphilipp/logicchips/items/Chip.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import lombok.val;
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -22,7 +23,7 @@ public class Chip extends Item {
     @Override
     public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> list, TooltipFlag tooltipFlag) {
         val langKey = Screen.hasShiftDown()
-            ? String.format("tooltip.%s.%s", LogicChips.MOD_ID, stack.getItem())
+            ? String.format("tooltip.%s.%s", LogicChips.MOD_ID, BuiltInRegistries.ITEM.getKey(stack.getItem()).getPath())
             : "tooltip.logicchips.shift";
         list.add(Component.translatable(langKey));
         super.appendHoverText(stack, context, list, tooltipFlag);
