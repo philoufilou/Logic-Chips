3d000fc9b75da1a4296b6812ed332d892491b7ad
ZZZank <3410764033@qq.com>
2024-10-19 00:28:29 +0800

    use name instead of recipe for setting DynamicChip logics

--- common/src/main/java/com/ichphilipp/logicchips/items/DynamicChip.java
+++ common/src/main/java/com/ichphilipp/logicchips/items/DynamicChip.java
@@ -1,5 +1,11 @@
 package com.ichphilipp.logicchips.items;
 
+import com.ichphilipp.logicchips.api.TriBoolLogic;
+import lombok.val;
+import net.minecraft.network.chat.Component;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
 /**
  * @author ZZZank
  */
@@ -7,5 +13,27 @@ public class DynamicChip extends Chip {
 
     public DynamicChip(Properties properties) {
         super(properties, ChipType.dynamic);
+    }
+
+    @Nullable
+    public static TriBoolLogic readLogicFromName(@NotNull Component displayName) {
+        val string = displayName.getString();
+        if (string.length() < 8) {
+            return null;
+        }
+        val logics = new boolean[8];
+        for (int i = 0; i < 8; i++) {
+            val c = string.charAt(i);
+            if (c == '0') {
+                logics[i] = false;
+            } else if (c == '1') {
+                logics[i] = true;
+            } else {
+                return null;
+            }
+        }
+        return (left, middle, right) -> left
+            ? middle ? right ? logics[0] : logics[1] : right ? logics[2] : logics[3]
+            : middle ? right ? logics[4] : logics[5] : right ? logics[6] : logics[7];
     }
 }
