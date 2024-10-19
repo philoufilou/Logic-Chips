c3bdb47eddce9a31106af3142811a41833c219cf
ZZZank <3410764033@qq.com>
2024-10-19 13:21:47 +0800

    make tooltip more in line with chip texture

--- common/src/main/java/com/ichphilipp/logicchips/items/DynamicChip.java
+++ common/src/main/java/com/ichphilipp/logicchips/items/DynamicChip.java
@@ -6,6 +6,7 @@ import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.network.chat.Style;
+import net.minecraft.network.chat.TextColor;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
@@ -37,18 +38,18 @@ public class DynamicChip extends Chip {
             return;
         }
         val logic = buildLogic(logicData);
-        val allBool = new boolean[]{true, false};
+        val allBool = new boolean[]{false, true};
         for (val left : allBool) {
             for (val mid : allBool) {
                 for (val right : allBool) {
                     val tip = Component.empty()
-                        .append(selectSignal(left, ChatFormatting.AQUA))
+                        .append(signal(left ? REGULAR_AQUA : DARK_AQUA))
                         .append(" + ")
-                        .append(selectSignal(mid, ChatFormatting.LIGHT_PURPLE))
+                        .append(signal(mid ? REGULAR_PURPLE : DARK_PURPLE))
                         .append(" + ")
-                        .append(selectSignal(right, ChatFormatting.YELLOW))
+                        .append(signal(right ? REGULAR_YELLOW : DARK_YELLOW))
                         .append(" -> ")
-                        .append(selectSignal(logic.apply(left, mid, right), ChatFormatting.RED))
+                        .append(signal(logic.apply(left, mid, right) ? REGULAR_RED : DARK_RED))
                     ;
                     tooltips.add(tip);
                 }
@@ -56,9 +57,21 @@ public class DynamicChip extends Chip {
         }
     }
 
-    private static MutableComponent selectSignal(boolean condition, ChatFormatting color) {
-        return Component.literal("█")
-            .setStyle(Style.EMPTY.withColor(condition ? color : ChatFormatting.GRAY));
+    private static final TextColor DARK_YELLOW = TextColor.parseColor("#404000");
+    private static final TextColor REGULAR_YELLOW = TextColor.fromLegacyFormat(ChatFormatting.YELLOW);
+    private static final TextColor DARK_AQUA = TextColor.parseColor("#004040");
+    private static final TextColor REGULAR_AQUA = TextColor.fromLegacyFormat(ChatFormatting.AQUA);
+    private static final TextColor DARK_PURPLE = TextColor.parseColor("#400040");
+    private static final TextColor REGULAR_PURPLE = TextColor.fromLegacyFormat(ChatFormatting.LIGHT_PURPLE);
+    private static final TextColor DARK_RED = TextColor.parseColor("#400000");
+    private static final TextColor REGULAR_RED = TextColor.fromLegacyFormat(ChatFormatting.RED);
+
+    private static @NotNull MutableComponent signal(ChatFormatting formatting) {
+        return Component.literal("█").setStyle(Style.EMPTY.withColor(formatting));
+    }
+
+    private static @NotNull MutableComponent signal(TextColor color) {
+        return Component.literal("█").setStyle(Style.EMPTY.withColor(color));
     }
 
     public static boolean @Nullable [] readLogicFromName(@NotNull Component displayName) {
