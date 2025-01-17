7f2302a7041d30d0fb9d08d946834b8b41c8836b
ZZZank <47418975+ZZZank@users.noreply.github.com>
2025-01-17 11:15:18 +0800

    Update Asser.java, there's no `CharSequence#isEmpty()` in Java8

--- common/src/main/java/com/ichphilipp/logicchips/utils/Asser.java
+++ common/src/main/java/com/ichphilipp/logicchips/utils/Asser.java
@@ -60,7 +60,7 @@ public class Asser {
 
     public <T extends CharSequence> T tNotEmpty(T value, String name) {
         tNotNull(value, name);
-        if (value.isEmpty()) {
+        if (value.size() == 0) {
             throw new NullPointerException("'" + name + "' must not be empty");
         }
         return value;
