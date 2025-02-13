df75bd4b161848eae482468dd62d16a008a8537b
ZZZank <3410764033@qq.com>
2025-02-13 22:46:01 +0800

    `Properties.setId()` is mandatory now, F you mojang

--- common/src/main/java/com/ichphilipp/logicchips/blocks/LogicChipsBlock.java
+++ common/src/main/java/com/ichphilipp/logicchips/blocks/LogicChipsBlock.java
@@ -2,6 +2,8 @@ package com.ichphilipp.logicchips.blocks;
 
 import com.ichphilipp.logicchips.utils.RegistryMgr;
 import dev.architectury.registry.registries.RegistrySupplier;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
@@ -11,6 +13,7 @@ import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Locale;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 public class LogicChipsBlock<T extends Block> implements Supplier<T> {
@@ -18,7 +21,8 @@ public class LogicChipsBlock<T extends Block> implements Supplier<T> {
 
     public static final LogicChipsBlock<ChipFrame> GATE_FRAME = new LogicChipsBlock<>(
         "gate_frame",
-        () -> new ChipFrame(BlockBehaviour.Properties.ofFullCopy(Blocks.REPEATER))
+        ChipFrame::new,
+        () -> BlockBehaviour.Properties.ofFullCopy(Blocks.REPEATER)
     );
 
     public static Map<String, LogicChipsBlock<?>> getAll() {
@@ -38,12 +42,27 @@ public class LogicChipsBlock<T extends Block> implements Supplier<T> {
         return this.item;
     }
 
-    private LogicChipsBlock(String name, Supplier<T> template) {
+    private LogicChipsBlock(
+        String name,
+        Function<BlockBehaviour.Properties, T> block,
+        Supplier<BlockBehaviour.Properties> properties
+    ) {
         this.name = name.toLowerCase(Locale.ROOT);
         if (ALL.containsKey(name)) {
             throw new IllegalArgumentException("already registered");
         }
-        this.block = RegistryMgr.BLOCK.register(this.name, template);
+        this.block = RegistryMgr.BLOCK.register(
+            this.name,
+            new Supplier<>() {
+                private final LogicChipsBlock<T> parent = LogicChipsBlock.this;
+
+                @Override
+                public T get() {
+                    return block.apply(properties.get()
+                        .setId(ResourceKey.create(Registries.BLOCK, parent.block.getId())));
+                }
+            }
+        );
         this.item = RegistryMgr.registerBlockItem(this.name, this.block);
         ALL.put(this.name, this);
     }
